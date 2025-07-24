a21a1527387bc239efe31fa12e47b29187237e82
Sohila010 <sohilaosman79@gmail.com>
2025-07-24 07:25:30 +0300

    update

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Footer from '../Footer/Footer'
+import avatar from '../../assets/avataaars.svg'
 
 export default function Home() {
   return <>
@@ -7,7 +8,7 @@ export default function Home() {
           <div className="container ">
               <div className="centralize d-flex justify-content-center align-items-center">
               <div className="cover w-25 mt-5 ">
-                      <img src="../../../images/avataaars.svg" className='w-100' alt="" />
+            <img src={avatar} className='w-100' alt="" />
                       <h3 className='text-center p-3 text-white fw-bolder'>start Framework</h3>
                   </div>
                   
